e7cc659cdba45309023d3ef2f8fc38e6c588d406
cachrisman <charlie.chrisman@gmail.com>
2015-05-29 10:43:02 -0700

    sorting phrases

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 //define database connection uristring for use with heroku
-var uristring = process.env.PROD_MONGODB || 'mongodb://localhost/catchphrasely_app';
+var uristring = process.env.PROD_MONGODB || process.env.DEV_MONGODB;
 mongoose.connect(uristring);
 
 //pass Phrases to export so index.js can access
